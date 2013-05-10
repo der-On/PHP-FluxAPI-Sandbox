a14160cb6ba5f6862650ab72cd7af8ddbf9fb1e8
Ondrej Brinkel <info@anzui.de>
2013-05-10 23:17:12 +0200

    replacing placeholder {$baseDir} in config json file in bootstrap now

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ $loader = require __DIR__ . '/../vendor/autoload.php';
 // create application
 $app = new Silex\Application();
 
-$config = json_decode(file_get_contents('../config/development.json'),TRUE);
+$config = json_decode(str_replace('{$baseDir}', realpath(__DIR__ . '/..'), file_get_contents('../config/development.json')),TRUE);
 
 if ($config['debug'] == TRUE) {
     $app['debug'] = TRUE;
